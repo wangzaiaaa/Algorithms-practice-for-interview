cefccc4e703ef792ec070f5881c8d1b36d995559
Doraemon <32564074+wangzaiaaa@users.noreply.github.com>
2019-04-29 20:02:16 +0800

    Update Problem8.java

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
-public class Problem8 {
+class MyTreeNode{
+    int val;
+    MyTreeNode parent,left,right;
+    MyTreeNode(int val,MyTreeNode parent){
+        this.val=val;
+        this.parent = parent;
+    }
+}
+public class Problem8{
+    public MyTreeNode getNext(MyTreeNode node){
+        if(node==null) return null;
+        MyTreeNode ans = null;
+        if(node.right!=null){
+            MyTreeNode right = node.right;
+            while (right.left!=null){
+                right = right.left;
+            }
+            ans = right;
+        }else if(node.parent!=null){
+            MyTreeNode cur = node;
+            MyTreeNode parent = node.parent;
+            while (parent!=null&&parent.right==cur){
+                cur = parent;
+                parent = cur.parent;
+            }
+            ans = parent;
+        }
+        return ans;
+    }
 }
