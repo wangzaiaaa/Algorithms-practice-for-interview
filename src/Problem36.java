e5eb711718a0bc452264e859029306371693b819
bonsoirzw <2627894288@qq.com>
2019-05-29 17:16:29 +0800

    Problem36

---
+++ src/Problem36.java
@@ -0,0 +1,35 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    TreeNode head = null;
+    TreeNode realHead = null;
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        ConvertSub(pRootOfTree);
+        return realHead;
+    }
+     
+    private void ConvertSub(TreeNode pRootOfTree) {
+        if(pRootOfTree==null) return;
+        ConvertSub(pRootOfTree.left);
+        if (head == null) {
+            head = pRootOfTree;
+            realHead = pRootOfTree;
+        } else {
+            head.right = pRootOfTree;
+            pRootOfTree.left = head;
+            head = pRootOfTree;
+        }
+        ConvertSub(pRootOfTree.right);
+    }
+}
